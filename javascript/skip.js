c66cefa146c337197bd190e8876da46e8858a3c6
pym <pablobie00@gmail.com>
2023-04-22 19:46:20 +0200

    pivot Index 2 index 4all files

--- javascript/skip.js
+++ javascript/skip.js
@@ -16,7 +16,7 @@
 
 			if ( element ) {
 				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
-					element.tabIndex = -1;
+					element.tabindex = -1;
 				}
 
 				element.focus();
